d8c32c3d87ece170f960885826aaa4e6762c29f1
Rick <mymiaojirui@gmail.com>
2023-10-23 14:18:14 +0800

    note

--- learn/base/class05/Code03_TreeMaxWidth.java
+++ learn/base/class05/Code03_TreeMaxWidth.java
@@ -2,75 +2,97 @@ package learn.base.class05;
 
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Queue;
 
 //求树的最大宽度
 public class Code03_TreeMaxWidth {
-
-	public static class Node {
-		public int value;
-		public Node left;
-		public Node right;
-
-		public Node(int data) {
-			this.value = data;
-		}
-	}
-
-	public static int getMaxWidth(Node head) {
-		if (head == null) {
-			return 0;
-		}
-		int maxWidth = 0;
-		int curWidth = 0;
-		int curLevel = 0;
-		HashMap<Node, Integer> levelMap = new HashMap<>();
-		levelMap.put(head, 1);
-		LinkedList<Node> queue = new LinkedList<>();
-		queue.add(head);
-		Node node = null;
-		Node left = null;
-		Node right = null;
-		//当队列不为空时
-		while (!queue.isEmpty()) {
-			//队列推出一个节点Node
-			node = queue.poll();
-			left = node.left;
-			right = node.right;
-			//左节点不为空时，放入levelMap，同时放入队列
-			if (left != null) {
-				levelMap.put(left, levelMap.get(node) + 1);
-				queue.add(left);
-			}
-			//右节点不为空时，放入levelMap，同时放入队列
-			if (right != null) {
-				levelMap.put(right, levelMap.get(node) + 1);
-				queue.add(right);
-			}
-			//当当前节点的序号大于curLevel时，令curWidth为0，curLevel设置为当前节点的序号
-			//否则curWidth++
-			if (levelMap.get(node) > curLevel) {
-				curWidth = 0;
-				curLevel = levelMap.get(node);
-			} else {
-				curWidth++;
-			}
-			//和当前最大值比较获取最大值
-			maxWidth = Math.max(maxWidth, curWidth);
-		}
-		return maxWidth;
-	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Node head = new Node(1);
-		head.left = new Node(2);
-		head.right = new Node(3);
-		head.left.left = new Node(4);
-		head.right.left = new Node(5);
-		head.right.right = new Node(6);
-		head.left.left.right = new Node(7);
-		int width = getMaxWidth(head);
-		System.out.println(width);
-	}
-
+    
+    public static class Node {
+        public int value;
+        public Node left;
+        public Node right;
+        
+        public Node(int data) {
+            this.value = data;
+        }
+    }
+    
+    // 层序遍历
+    public static void levelOrderTraversal(Node head) {
+        if (head == null) {
+            return;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(head);
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
+            System.out.print(node.value + " ");
+            if (node.left != null) {
+                queue.add(node.left);
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+        }
+    }
+    
+    public static int getMaxWidth(Node head) {
+        if (head == null) {
+            return 0;
+        }
+        int maxWidth = 0;
+        int curWidth = 0;
+        int curLevel = 0;
+        HashMap<Node, Integer> levelMap = new HashMap<>();
+        levelMap.put(head, 1);
+        LinkedList<Node> queue = new LinkedList<>();
+        queue.add(head);
+        Node node = null;
+        Node left = null;
+        Node right = null;
+        //当队列不为空时
+        while (!queue.isEmpty()) {
+            //队列推出一个节点Node
+            node = queue.poll();
+            left = node.left;
+            right = node.right;
+            //左节点不为空时，放入levelMap，同时放入队列
+            if (left != null) {
+                levelMap.put(left, levelMap.get(node) + 1);
+                queue.add(left);
+            }
+            //右节点不为空时，放入levelMap，同时放入队列
+            if (right != null) {
+                levelMap.put(right, levelMap.get(node) + 1);
+                queue.add(right);
+            }
+            //当当前节点的序号大于curLevel时，令curWidth为0，curLevel设置为当前节点的序号
+            //否则curWidth++
+            if (levelMap.get(node) > curLevel) {
+                curWidth = 0;
+                curLevel = levelMap.get(node);
+            } else {
+                curWidth++;
+            }
+            //和当前最大值比较获取最大值
+            maxWidth = Math.max(maxWidth, curWidth);
+        }
+        return maxWidth;
+    }
+    
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.left = new Node(2);
+        head.right = new Node(3);
+        head.left.left = new Node(4);
+        head.right.left = new Node(5);
+        head.right.right = new Node(6);
+        head.left.left.right = new Node(7);
+        System.out.println("层序遍历:");
+        levelOrderTraversal(head);
+        System.out.println("\n" + "=======获取最大宽度=======");
+        int width = getMaxWidth(head);
+        System.out.println(width);
+    }
+    
 }
--- learn/base/class05/Code04_IsBST.java
+++ learn/base/class05/Code04_IsBST.java
@@ -1,45 +1,137 @@
 package learn.base.class05;
 
 import java.util.LinkedList;
+import java.util.Stack;
 
-//判断是否是二叉排序树
-//左节点小于根节点，右节点大于根节点
+// 判断是否是二叉排序树
+// 左节点小于根节点，右节点大于根节点
 public class Code04_IsBST {
-
-	public static class Node {
-		public int value;
-		public Node left;
-		public Node right;
-
-		public Node(int data) {
-			this.value = data;
-		}
-	}
-
-	public static boolean isBST(Node head) {
-		if (head == null) {
-			return true;
-		}
-		LinkedList<Node> inOrderList = new LinkedList<>();
-		process(head, inOrderList);
-		int pre = Integer.MIN_VALUE;
-		for (Node cur : inOrderList) {
-			if (pre >= cur.value) {
-				return false;
-			}
-			pre = cur.value;
-		}
-		return true;
-	}
-
-	//中序递归遍历
-	public static void process(Node node, LinkedList<Node> inOrderList) {
-		if (node == null) {
-			return;
-		}
-		process(node.left, inOrderList);
-		inOrderList.add(node);
-		process(node.right, inOrderList);
-	}
-
+    
+    public static class Node {
+        public int value;
+        public Node left;
+        public Node right;
+        
+        public Node(int data) {
+            this.value = data;
+        }
+    }
+    
+    private static int preValue1 = Integer.MIN_VALUE;
+    
+    // 1. 递归中序遍历 动态检查
+    public static boolean isBST1(Node head) {
+        if (head == null) {
+            return true;
+        }
+        if (!isBST1(head.left)) {
+            return false;
+        }
+        if (head.value > preValue1) {
+            preValue1 = head.value;
+        } else {
+            return false;
+        }
+        return isBST1(head.right);
+    }
+    
+    // 2. 非递归中序遍历 动态检查
+    public static boolean isBST2(Node head) {
+        if (head == null) {
+            return true;
+        }
+        Stack<Node> stack = new Stack<>();
+        int preValue2 = Integer.MIN_VALUE;
+        while (!stack.isEmpty() || head != null) {
+            if (head != null) {
+                stack.push(head);
+                head = head.left;
+            } else {
+                head = stack.pop();
+                if (head.value > preValue2) {
+                    preValue2 = head.value;
+                } else {
+                    return false;
+                }
+                head = head.right;
+            }
+        }
+        return true;
+    }
+    
+    // 3. 傻白甜代码
+    public static boolean isBST3(Node head) {
+        if (head == null) {
+            return true;
+        }
+        LinkedList<Node> inOrderList = new LinkedList<>();
+        process(head, inOrderList);
+        int pre = Integer.MIN_VALUE;
+        for (Node cur : inOrderList) {
+            if (pre >= cur.value) {
+                return false;
+            }
+            pre = cur.value;
+        }
+        return true;
+    }
+    
+    // 4. 套路解 找特征每个节点都要返回 是否二叉搜索树，最大值，最小值
+    public static ReturnType isBST4(Node head) {
+        if (head == null) {
+            return new ReturnType(true, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        }
+        ReturnType leftData = isBST4(head.left);
+        ReturnType rightData = isBST4(head.right);
+        boolean flag = (leftData.flag && rightData.flag) && head.value > leftData.max && head.value < rightData.min;
+        int max = Math.max(leftData.max, head.value);
+        int min = Math.min(rightData.min, head.value);
+        return new ReturnType(flag, max, min);
+    }
+    
+    public static class ReturnType {
+        public boolean flag;
+        public int max;
+        public int min;
+    
+        public ReturnType(boolean flag, int max, int min) {
+            this.flag = flag;
+            this.max = max;
+            this.min = min;
+        }
+    }
+    
+    // 中序递归遍历
+    public static void process(Node node, LinkedList<Node> inOrderList) {
+        if (node == null) {
+            return;
+        }
+        process(node.left, inOrderList);
+        inOrderList.add(node);
+        process(node.right, inOrderList);
+    }
+    
+    public static void main(String[] args) {
+        Node head = new Node(5);
+        head.left = new Node(3);
+        head.right = new Node(8);
+        head.left.left = new Node(2);
+        head.left.right = new Node(4);
+        head.left.left.left = new Node(1);
+        head.right.left = new Node(7);
+        head.right.left.left = new Node(6);
+        head.right.right = new Node(10);
+        head.right.right.left = new Node(9);
+        head.right.right.right = new Node(11);
+        
+        boolean isBST1 = isBST1(head);
+        System.out.println("isBST1:" + isBST1);
+        boolean isBST2 = isBST2(head);
+        System.out.println("isBST2:" + isBST2);
+        boolean isBST3 = isBST3(head);
+        System.out.println("isBST3:" + isBST3);
+        boolean isBST4 = isBST4(head).flag;
+        System.out.println("isBST4:" + isBST4);
+    }
+    
 }
