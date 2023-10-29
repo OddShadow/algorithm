26897efc4e83b086141f44249e8dd3fc128bc7ec
Rick <mymiaojirui@gmail.com>
2023-10-29 12:09:10 +0800

    note

--- learn/base/class06/Code01_BFS.java
+++ learn/base/class06/Code01_BFS.java
@@ -4,13 +4,13 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
 
-//图的宽度优先遍历
-//1，利用队列实现
-//2，从源节点开始依次按照宽度进队列，然后弹出
-//3，每弹出一个点，把该节点所有没有进过队列的邻接点放入队列
-//4，直到队列变空
+// 图的宽度优先遍历
+// 1，利用队列实现
+// 2，从源节点开始依次按照宽度进队列，然后弹出
+// 3，每弹出一个点，把该节点所有没有进过队列的邻接点放入队列
+// 4，直到队列变空
 public class Code01_BFS {
-    
+
     public static void bfs(Node node) {
         if (node == null) {
             return;
@@ -21,7 +21,7 @@ public class Code01_BFS {
         queue.add(node);
         map.add(node);
         //当队列不为空时，令cur为队列推出的一个
-        
+
         while (!queue.isEmpty()) {
             Node cur = queue.poll();
             System.out.println(cur.value);
@@ -35,5 +35,5 @@ public class Code01_BFS {
             }
         }
     }
-    
+
 }
