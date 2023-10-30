eb013f993a0a2f633b4d6b2b0713cb75d8d40099
Rick <mymiaojirui@gmail.com>
2023-10-31 01:07:03 +0800

    note

--- learn/base/class08/Code01_Hanoi.java
+++ learn/base/class08/Code01_Hanoi.java
@@ -1,34 +1,32 @@
 package learn.base.class08;
 
-//汉诺塔问题
-//
-//down：减少的
+// 汉诺塔问题，左中右三个盘，从左移到右
 public class Code01_Hanoi {
 
-	public static void hanoi(int n) {
-		if (n > 0) {
-			func(n, n, "left", "mid", "right");
-		}
-	}
+    public static void hanoi(int n) {
+        if (n > 0) {
+            func(n, n, "left", "mid", "right");
+        }
+    }
 
-	// 1~i 圆盘 目标是from -> to,other是另外一个
-	public static void func(int rest, int down, String from, String help, String to) {
-		//如果剩余只有1块底，就把这一块移动到to
-		if (rest == 1) {
-			System.out.println("move " + down + " from " + from + " to " + to);
-		} else {
-			//将rest-1从from移动到help
-			func(rest - 1, down - 1, from, to, help);
-			//将最底下一块从from移动到to
-			func(1, down, from, help, to);
-			//将rest-1从help移动到to
-			func(rest - 1, down - 1, help, from, to);
-		}
-	}
+    // 1~i 从小到大的圆盘 目标是from盘 -> to盘, help 是辅助盘
+    public static void func(int rest, int down, String from, String help, String to) {
+        // 如果剩余只有1块底，就把这一块移动到to
+        if (rest == 1) {
+            System.out.println("move " + down + " from " + from + " to " + to);
+        } else {
+            // 将rest-1从from移动到help
+            func(rest - 1, down - 1, from, to, help);
+            // 将最底下一块从from移动到to
+            func(1, down, from, help, to);
+            // 将rest-1从help移动到to
+            func(rest - 1, down - 1, help, from, to);
+        }
+    }
 
-	public static void main(String[] args) {
-		int n = 4;
-		hanoi(n);
-	}
+    public static void main(String[] args) {
+        int n = 4;
+        hanoi(n);
+    }
 
 }
--- learn/base/class08/Code09_NQueens.java
+++ learn/base/class08/Code09_NQueens.java
@@ -2,76 +2,82 @@ package learn.base.class08;
 
 public class Code09_NQueens {
 
-	public static int num1(int n) {
-		if (n < 1) {
-			return 0;
-		}
-		int[] record = new int[n];
-		return process1(0, record, n);
-	}
+    public static int num1(int n) {
+        if (n < 1) {
+            return 0;
+        }
+        int[] record = new int[n];
+        return process1(0, record, n);
+    }
 
-	public static int process1(int i, int[] record, int n) {
-		if (i == n) {
-			return 1;
-		}
-		int res = 0;
-		for (int j = 0; j < n; j++) {
-			if (isValid(record, i, j)) {
-				record[i] = j;
-				res += process1(i + 1, record, n);
-			}
-		}
-		return res;
-	}
+    // i 第几行 0 ~ n-1
+    // 所有皇后位置 record[0]=0 表示，0行皇后在0列
+    // n 表示有几个皇后，n*n 的棋盘
+    // 返回值表示多少种摆放方式
+    public static int process1(int i, int[] record, int n) {
+        if (i == n) { // 终止行
+            return 1;
+        }
+        int res = 0;
+        for (int j = 0; j < n; j++) {
+            if (isValid(record, i, j)) {
+                record[i] = j;
+                res += process1(i + 1, record, n);
+            }
+        }
+        return res;
+    }
 
-	public static boolean isValid(int[] record, int i, int j) {
-		for (int k = 0; k < i; k++) {
-			if (j == record[k] || Math.abs(record[k] - j) == Math.abs(i - k)) {
-				return false;
-			}
-		}
-		return true;
-	}
+    public static boolean isValid(int[] record, int i, int j) {
+        for (int k = 0; k < i; k++) {
+            if (j == record[k] || Math.abs(record[k] - j) == Math.abs(i - k)) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-	public static int num2(int n) {
-		if (n < 1 || n > 32) {
-			return 0;
-		}
-		int upperLim = n == 32 ? -1 : (1 << n) - 1;
-		return process2(upperLim, 0, 0, 0);
-	}
+    public static int num2(int n) {
+        if (n < 1 || n > 32) {
+            return 0;
+        }
+        int limit = n == 32 ? -1 : (1 << n) - 1;
+        return process2(limit, 0, 0, 0);
+    }
 
-	public static int process2(int upperLim, int colLim, int leftDiaLim,
-			int rightDiaLim) {
-		if (colLim == upperLim) {
-			return 1;
-		}
-		int pos = 0;
-		int mostRightOne = 0;
-		pos = upperLim & (~(colLim | leftDiaLim | rightDiaLim));
-		int res = 0;
-		while (pos != 0) {
-			mostRightOne = pos & (~pos + 1);
-			pos = pos - mostRightOne;
-			res += process2(upperLim, colLim | mostRightOne,
-					(leftDiaLim | mostRightOne) << 1,
-					(rightDiaLim | mostRightOne) >>> 1);
-		}
-		return res;
-	}
+    // limit 皇后数的位表示，8 皇后即 0000...0000 1111 1111
+    // colLim 列限制 0000 0010 即表示右边第二位不可以放置
+    // leftDiaLim  左对角线限制 0000 0100 即表示右边第三位不可以放置
+    // rightDiaLim 右对角线限制 0000 0001 即表示右边第一位不可以放置
+    public static int process2(int limit, int colLim, int leftDiaLim, int rightDiaLim) {
+        if (colLim == limit) {
+            return 1;
+        }
+        int pos = limit & (~(colLim | leftDiaLim | rightDiaLim)); // 净化超出位置为0，1表示可以放置位置
+        int res = 0;
+        while (pos != 0) {
+            int mostRightOne = pos & (~pos + 1); // 取最右可放置位
+            pos = pos - mostRightOne;
+            res += process2(limit,
+                    colLim | mostRightOne,
+                    (leftDiaLim | mostRightOne) << 1,
+                    (rightDiaLim | mostRightOne) >>> 1);
+        }
+        return res;
+    }
 
-	public static void main(String[] args) {
-		int n = 14;
+    public static void main(String[] args) {
+        int n = 4;
 
-		long start = System.currentTimeMillis();
-		System.out.println(num2(n));
-		long end = System.currentTimeMillis();
-		System.out.println("cost time: " + (end - start) + "ms");
+        long start = System.currentTimeMillis();
+        System.out.println(num2(n));
+        long end = System.currentTimeMillis();
+        System.out.println("cost time: " + (end - start) + "ms");
 
-		start = System.currentTimeMillis();
-		System.out.println(num1(n));
-		end = System.currentTimeMillis();
-		System.out.println("cost time: " + (end - start) + "ms");
+        start = System.currentTimeMillis();
+        System.out.println(num1(n));
+        end = System.currentTimeMillis();
+        System.out.println("cost time: " + (end - start) + "ms");
 
-	}
+    }
 }
