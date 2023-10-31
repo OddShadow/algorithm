0cb1b40323e47c9f3e8c66b46b811963fdd8d1ef
Rick <mymiaojirui@gmail.com>
2023-10-31 20:22:43 +0800

    note

--- learn/base/class08/Code02_PrintAllSubsequences.java
+++ learn/base/class08/Code02_PrintAllSubsequences.java
@@ -1,66 +1,63 @@
 package learn.base.class08;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 // 打印一个字符串的全部子序列，包括空字符串
-public class Code02_PrintAllSubsquences {
+public class Code02_PrintAllSubsequences {
 
-    public static void printAllSubsquence(String str) {
+    public static void printAllSubsequence1(String str) {
         char[] chs = str.toCharArray();
-        process(chs, 0);
+        process1(chs, 0);
     }
 
     // 当前来到i位置
-    public static void process(char[] chs, int i) {
+    public static void process1(char[] chs, int i) {
         // 如果i已经抵达终止位置，直接打印值
         if (i == chs.length) {
             System.out.println(String.valueOf(chs));
             return;
         }
         // 新增下一个字符进入考虑范围，并把之前的字符串也考虑进去
-        process(chs, i + 1);
+        process1(chs, i + 1);
         char tmp = chs[i];
         chs[i] = 0;
         // 不要当前字符的路
-        process(chs, i + 1);
+        process1(chs, i + 1);
         chs[i] = tmp;
     }
 
-    public static void function(String str) {
+    public static void printAllSubsequence2(String str) {
         char[] chs = str.toCharArray();
-        process(chs, 0, new ArrayList<Character>());
+        process2(chs, 0, new ArrayList<>());
     }
 
     // 当前来到i位置，要和不要，走两条路
     // res 之前的选择，所形成的列表
-    public static void process(char[] chs, int i, List<Character> res) {
+    public static void process2(char[] chs, int i, List<Character> res) {
         if (i == chs.length) {
             printList(res);
+            return;
         }
         List<Character> resKeep = copyList(res);
         resKeep.add(chs[i]);
-        process(chs, i + 1, resKeep);
+        process2(chs, i + 1, resKeep);
         List<Character> resNoInclude = copyList(res);
-        process(chs, i + 1, resNoInclude);
+        process2(chs, i + 1, resNoInclude);
     }
 
     public static void printList(List<Character> res) {
-        // ...;
+        System.out.println(res);
     }
 
     public static List<Character> copyList(List<Character> list) {
-        return null;
+        return new ArrayList<>(list);
     }
 
-
     public static void main(String[] args) {
-        String test = "abcb";
-        printAllSubsquence(test);
+        String test = "abc";
+        printAllSubsequence1(test);
+//        printAllSubsequence2(test);
     }
 
 }
