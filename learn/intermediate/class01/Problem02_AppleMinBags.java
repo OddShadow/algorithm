3dcc7f9df08f7bfaa4d45c01649d8eaeddde7b23
Rick <mymiaojirui@gmail.com>
2023-12-04 21:40:59 +0800

    note

--- learn/intermediate/class01/Problem02_AppleMinBags.java
+++ learn/intermediate/class01/Problem02_AppleMinBags.java
@@ -1,50 +1,76 @@
 package learn.intermediate.class01;
 
+/*
+    小虎去附近的商店买苹果，奸诈的商贩使用了捆绑交易
+    只提供 6 个每袋和 8 个每袋的包装包装，不可拆分
+    可是小虎现在只想购买恰好 n 个苹果，小虎想购买尽量少的袋数，方便携带
+    如果不能购买恰好 n 个苹果，小虎将不会购买
+    输入一个整数 n，表示小虎想购买的苹果数，返回最小使用多少袋子
+    如果无论如何都不能正好装下，返回 -1
+    
+    思考 6x+8y=n
+    那么最小公倍数 24 即可以用 6a 表示，也可也用 8b 表示，题意肯定用 8b 表示
+    贪心用 bag8 先装，剩 >24 个后，想用 bag6 装就没必要了
+    
+    循环打表，简简单单，嘟嘟嘟
+ */
 public class Problem02_AppleMinBags {
-
-	public static int minBags(int apple) {
-		if (apple < 0) {
-			return -1;
-		}
-		int bag6 = -1;
-		int bag8 = apple / 8;
-		int rest = apple - 8 * bag8;
-		while (bag8 >= 0 && rest < 24) {
-			int restUse6 = minBagBase6(rest);
-			if (restUse6 != -1) {
-				bag6 = restUse6;
-				break;
-			}
-			rest = apple - 8 * (--bag8);
-		}
-		return bag6 == -1 ? -1 : bag6 + bag8;
-	}
-
-	public static int minBagBase6(int rest) {
-		return rest % 6 == 0 ? (rest / 6) : -1;
-	}
-
-	public static int minBagAwesome(int apple) {
-		if ((apple & 1) != 0) {
-			return -1;
-		}
-		if (apple < 18) {
-			return apple == 0 ? 0 : (apple == 6 || apple == 8) ? 1
-					: (apple == 12 || apple == 14 || apple == 16) ? 2 : -1;
-		}
-		return (apple - 18) / 8 + 3;
-	}
-
-	public static void main(String[] args) {
-		int max = Integer.MAX_VALUE;
-		int testTime = 100000000;
-		for (int test = 0; test < testTime; test++) {
-			int apple = (int) (Math.random() * max);
-			if (minBags(apple) != minBagAwesome(apple)) {
-				System.out.println("error");
-			}
-		}
-
-	}
-
+    
+    public static int minBags(int apple) {
+        if (apple < 0) {
+            return -1;
+        }
+        int bag6 = -1;
+        int bag8 = apple / 8;
+        int rest = apple - 8 * bag8;
+        while (bag8 >= 0 && rest < 24) {
+            int restUse6 = minBagBase6(rest);
+            if (restUse6 != -1) {
+                bag6 = restUse6;
+                break;
+            }
+            rest = apple - 8 * (--bag8);
+        }
+        return bag6 == -1 ? -1 : bag6 + bag8;
+    }
+    
+    public static int minBagBase6(int rest) {
+        return rest % 6 == 0 ? (rest / 6) : -1;
+    }
+    
+    public static int minBagAwesome(int apple) {
+        if ((apple & 1) != 0) {
+            return -1;
+        }
+        if (apple < 18) {
+            return apple == 0
+                           ? 0
+                           : (apple == 6 || apple == 8)
+                                     ? 1
+                                     : (apple == 12 || apple == 14 || apple == 16)
+                                               ? 2
+                                               : -1;
+        }
+        return (apple - 18) / 8 + 3;
+    }
+    
+    public static void main(String[] args) {
+        getRegular(); // 打表
+        int max = Integer.MAX_VALUE;
+        int testTime = 100000000;
+        for (int test = 0; test < testTime; test++) {
+            int apple = (int) (Math.random() * max);
+            if (minBags(apple) != minBagAwesome(apple)) {
+                System.out.println("error");
+            }
+        }
+        
+    }
+    
+    public static void getRegular() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i + "::" +minBags(i));
+        }
+    }
+    
 }
--- learn/intermediate/class02/Problem06_Eat.java
+++ learn/intermediate/class02/Problem06_Eat.java
@@ -1,13 +1,54 @@
 package learn.intermediate.class02;
 
+/*
+    n 份青草放一堆
+    先手-first 后手-second 都聪明
+    每次只能吃 4 的 n 次方份草
+    谁最后把草吃完谁赢
+ */
 public class Problem06_Eat {
-
-	public static void printWinner(int n) {
-		if (n % 5 == 0 || n % 5 == 2) {
-			System.out.println("yang");
-		} else {
-			System.out.println("niu");
-		}
-	}
-
+    
+    public static String solution1(int n) {
+        if (n <= 4) {
+            return (n == 0 || n == 2) ? "second" : "first";
+        }
+        int base = 1; // 1 4 16 64 ...
+        while (base <= n) {
+            if (solution1(n - base).equals("second")) {
+                return "first";
+            }
+            if (base > n / 4) { // 防止数据越界写法
+                break;
+            }
+            base *= 4;
+        }
+        return "second";
+    }
+    
+    public static String solution2(int n) {
+        if (n % 5 == 0 || n % 5 == 2) {
+            return "second";
+        } else {
+            return "first";
+        }
+    }
+    
+    public static void main(String[] args) {
+        getRegular();
+        
+        for (int i = 0; i < 60; i++) {
+            if (!solution1(i).equals(solution2(i))) {
+                System.out.println("Fuck!!!");
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+    
+    public static void getRegular() {
+        for (int i = 0; i < 60; i++) {
+            System.out.printf("%3d::%s\r\n", i, solution1(i));
+        }
+    }
+    
 }
