691f5e541e6354796746f5eed08e52daad8857b4
Rick <mymiaojirui@gmail.com>
2023-12-06 20:23:29 +0800

    note

--- learn/intermediate/class03/Problem03_RotateMatrix.java
+++ learn/intermediate/class03/Problem03_RotateMatrix.java
@@ -1,46 +1,59 @@
 package learn.intermediate.class03;
 
+/*
+    一个正方形矩阵，只用几个有限变量，实现矩阵中每个位置的数顺时针转动90度
+    原矩阵
+        0  1  2  3
+        4  5  6  7
+        8  9  10 11
+        12 13 14 15
+    矩阵变成
+        12 8  4  0
+        13 9  5  1
+        14 10 6  2
+        15 11 7  3
+ */
 public class Problem03_RotateMatrix {
-
-	public static void rotate(int[][] matrix) {
-		int tR = 0;
-		int tC = 0;
-		int dR = matrix.length - 1;
-		int dC = matrix[0].length - 1;
-		while (tR < dR) {
-			rotateEdge(matrix, tR++, tC++, dR--, dC--);
-		}
-	}
-
-	public static void rotateEdge(int[][] m, int tR, int tC, int dR, int dC) {
-		int times = dC - tC; 
-		int tmp = 0;
-		for (int i = 0; i != times; i++) {
-			tmp = m[tR][tC + i];
-			m[tR][tC + i] = m[dR - i][tC];
-			m[dR - i][tC] = m[dR][dC - i];
-			m[dR][dC - i] = m[tR + i][dC];
-			m[tR + i][dC] = tmp;
-		}
-	}
-
-	public static void printMatrix(int[][] matrix) {
-		for (int i = 0; i != matrix.length; i++) {
-			for (int j = 0; j != matrix[0].length; j++) {
-				System.out.print(matrix[i][j] + " ");
-			}
-			System.out.println();
-		}
-	}
-
-	public static void main(String[] args) {
-		int[][] matrix = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 },
-				{ 13, 14, 15, 16 } };
-		printMatrix(matrix);
-		rotate(matrix);
-		System.out.println("=========");
-		printMatrix(matrix);
-
-	}
-
+    
+    public static void rotate(int[][] matrix) {
+        int tR = 0;
+        int tC = 0;
+        int dR = matrix.length - 1;
+        int dC = matrix[0].length - 1;
+        while (tR < dR) {
+            rotateEdge(matrix, tR++, tC++, dR--, dC--);
+        }
+    }
+    
+    public static void rotateEdge(int[][] m, int tR, int tC, int dR, int dC) {
+        int times = dC - tC;
+        int tmp = 0;
+        for (int i = 0; i != times; i++) {
+            tmp = m[tR][tC + i];
+            m[tR][tC + i] = m[dR - i][tC];
+            m[dR - i][tC] = m[dR][dC - i];
+            m[dR][dC - i] = m[tR + i][dC];
+            m[tR + i][dC] = tmp;
+        }
+    }
+    
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i != matrix.length; i++) {
+            for (int j = 0; j != matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12},
+                {13, 14, 15, 16}};
+        printMatrix(matrix);
+        rotate(matrix);
+        System.out.println("=========");
+        printMatrix(matrix);
+        
+    }
+    
 }
