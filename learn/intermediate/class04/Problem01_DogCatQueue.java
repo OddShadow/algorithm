fbae5fe4390d49372e389b1354e3268b4b5c8a9c
Rick <mymiaojirui@gmail.com>
2023-12-06 23:58:14 +0800

    note

--- learn/intermediate/class04/Problem01_DogCatQueue.java
+++ learn/intermediate/class04/Problem01_DogCatQueue.java
@@ -3,156 +3,161 @@ package learn.intermediate.class04;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/*
+    实现一种狗猫队列的结构，要求如下:
+    用户可以调用 add 方法将 cat 类或 dog 类的实例放入队列中;
+    用户可以调用 pollAll 方法，将队列中所有的实例按照进队列的先后顺序依次弹出;
+    用户可以调用 pollDog 方法，将队列中dog类的实例按照进队列的先后顺序依次弹出;
+    用户可以调用 pollCat 方法，将队列中cat类的实 例按照进队列的先后顺序依次弹出;
+    用户可以调用 isEmpty 方法，检查队列中是否还有 dog 或 cat 的实例;
+    用户可以调用 isDogEmpty 方法，检查队列中是否有 dog 类的实例;
+    用户可以调用 isCatEmpty 方法，检查队列中是否有 cat 类的实例;
+    要求以上所有方法时间复杂度都是 O(1) 的
+ */
 public class Problem01_DogCatQueue {
-
-	public static class Pet {
-		private String type;
-
-		public Pet(String type) {
-			this.type = type;
-		}
-
-		public String getPetType() {
-			return this.type;
-		}
-	}
-
-	public static class Dog extends Pet {
-		public Dog() {
-			super("dog");
-		}
-	}
-
-	public static class Cat extends Pet {
-		public Cat() {
-			super("cat");
-		}
-	}
-
-	public static class PetEnterQueue {
-		private Pet pet;
-		private long count;
-
-		public PetEnterQueue(Pet pet, long count) {
-			this.pet = pet;
-			this.count = count;
-		}
-
-		public Pet getPet() {
-			return this.pet;
-		}
-
-		public long getCount() {
-			return this.count;
-		}
-
-		public String getEnterPetType() {
-			return this.pet.getPetType();
-		}
-	}
-
-	public static class DogCatQueue {
-		private Queue<PetEnterQueue> dogQ;
-		private Queue<PetEnterQueue> catQ;
-		private long count;
-
-		public DogCatQueue() {
-			this.dogQ = new LinkedList<PetEnterQueue>();
-			this.catQ = new LinkedList<PetEnterQueue>();
-			this.count = 0;
-		}
-
-		public void add(Pet pet) {
-			if (pet.getPetType().equals("dog")) {
-				this.dogQ.add(new PetEnterQueue(pet, this.count++));
-			} else if (pet.getPetType().equals("cat")) {
-				this.catQ.add(new PetEnterQueue(pet, this.count++));
-			} else {
-				throw new RuntimeException("err, not dog or cat");
-			}
-		}
-
-		public Pet pollAll() {
-			if (!this.dogQ.isEmpty() && !this.catQ.isEmpty()) {
-				if (this.dogQ.peek().getCount() < this.catQ.peek().getCount()) {
-					return this.dogQ.poll().getPet();
-				} else {
-					return this.catQ.poll().getPet();
-				}
-			} else if (!this.dogQ.isEmpty()) {
-				return this.dogQ.poll().getPet();
-			} else if (!this.catQ.isEmpty()) {
-				return this.catQ.poll().getPet();
-			} else {
-				throw new RuntimeException("err, queue is empty!");
-			}
-		}
-
-		public Dog pollDog() {
-			if (!this.isDogQueueEmpty()) {
-				return (Dog) this.dogQ.poll().getPet();
-			} else {
-				throw new RuntimeException("Dog queue is empty!");
-			}
-		}
-
-		public Cat pollCat() {
-			if (!this.isCatQueueEmpty()) {
-				return (Cat) this.catQ.poll().getPet();
-			} else
-				throw new RuntimeException("Cat queue is empty!");
-		}
-
-		public boolean isEmpty() {
-			return this.dogQ.isEmpty() && this.catQ.isEmpty();
-		}
-
-		public boolean isDogQueueEmpty() {
-			return this.dogQ.isEmpty();
-		}
-
-		public boolean isCatQueueEmpty() {
-			return this.catQ.isEmpty();
-		}
-
-	}
-
-	public static void main(String[] args) {
-		DogCatQueue test = new DogCatQueue();
-
-		Pet dog1 = new Dog();
-		Pet cat1 = new Cat();
-		Pet dog2 = new Dog();
-		Pet cat2 = new Cat();
-		Pet dog3 = new Dog();
-		Pet cat3 = new Cat();
-
-		test.add(dog1);
-		test.add(cat1);
-		test.add(dog2);
-		test.add(cat2);
-		test.add(dog3);
-		test.add(cat3);
-
-		test.add(dog1);
-		test.add(cat1);
-		test.add(dog2);
-		test.add(cat2);
-		test.add(dog3);
-		test.add(cat3);
-
-		test.add(dog1);
-		test.add(cat1);
-		test.add(dog2);
-		test.add(cat2);
-		test.add(dog3);
-		test.add(cat3);
-		while (!test.isDogQueueEmpty()) {
-			System.out.println(test.pollDog().getPetType());
-		}
-		while (!test.isEmpty()) {
-			System.out.println(test.pollAll().getPetType());
-		}
-	}
-
+    
+    public static class Pet {
+        private String type;
+        public Pet(String type) {
+            this.type = type;
+        }
+        public String getPetType() {
+            return this.type;
+        }
+    }
+    
+    public static class Dog extends Pet {
+        public Dog() {
+            super("dog");
+        }
+    }
+    
+    public static class Cat extends Pet {
+        public Cat() {
+            super("cat");
+        }
+    }
+    
+    public static class PetEnterQueue {
+        private Pet pet;
+        private long count;
+        public PetEnterQueue(Pet pet, long count) {
+            this.pet = pet;
+            this.count = count;
+        }
+        public Pet getPet() {
+            return this.pet;
+        }
+        public long getCount() {
+            return this.count;
+        }
+        public String getEnterPetType() {
+            return this.pet.getPetType();
+        }
+    }
+    
+    public static class DogCatQueue {
+        private Queue<PetEnterQueue> dogQ;
+        private Queue<PetEnterQueue> catQ;
+        private long count;
+        
+        public DogCatQueue() {
+            this.dogQ = new LinkedList<>();
+            this.catQ = new LinkedList<>();
+            this.count = 0;
+        }
+        
+        public void add(Pet pet) {
+            if (pet.getPetType().equals("dog")) {
+                this.dogQ.add(new PetEnterQueue(pet, this.count++));
+            } else if (pet.getPetType().equals("cat")) {
+                this.catQ.add(new PetEnterQueue(pet, this.count++));
+            } else {
+                throw new RuntimeException("err, not dog or cat");
+            }
+        }
+        
+        public Pet pollAll() {
+            if (!this.dogQ.isEmpty() && !this.catQ.isEmpty()) {
+                if (this.dogQ.peek().getCount() < this.catQ.peek().getCount()) {
+                    return this.dogQ.poll().getPet();
+                } else {
+                    return this.catQ.poll().getPet();
+                }
+            } else if (!this.dogQ.isEmpty()) {
+                return this.dogQ.poll().getPet();
+            } else if (!this.catQ.isEmpty()) {
+                return this.catQ.poll().getPet();
+            } else {
+                throw new RuntimeException("err, queue is empty!");
+            }
+        }
+        
+        public Dog pollDog() {
+            if (!this.isDogQueueEmpty()) {
+                return (Dog) this.dogQ.poll().getPet();
+            } else {
+                throw new RuntimeException("Dog queue is empty!");
+            }
+        }
+        
+        public Cat pollCat() {
+            if (!this.isCatQueueEmpty()) {
+                return (Cat) this.catQ.poll().getPet();
+            } else
+                throw new RuntimeException("Cat queue is empty!");
+        }
+        
+        public boolean isEmpty() {
+            return this.dogQ.isEmpty() && this.catQ.isEmpty();
+        }
+        
+        public boolean isDogQueueEmpty() {
+            return this.dogQ.isEmpty();
+        }
+        
+        public boolean isCatQueueEmpty() {
+            return this.catQ.isEmpty();
+        }
+        
+    }
+    
+    public static void main(String[] args) {
+        DogCatQueue test = new DogCatQueue();
+        
+        Pet dog1 = new Dog();
+        Pet cat1 = new Cat();
+        Pet dog2 = new Dog();
+        Pet cat2 = new Cat();
+        Pet dog3 = new Dog();
+        Pet cat3 = new Cat();
+        
+        test.add(dog1);
+        test.add(cat1);
+        test.add(dog2);
+        test.add(cat2);
+        test.add(dog3);
+        test.add(cat3);
+        
+        test.add(dog1);
+        test.add(cat1);
+        test.add(dog2);
+        test.add(cat2);
+        test.add(dog3);
+        test.add(cat3);
+        
+        test.add(dog1);
+        test.add(cat1);
+        test.add(dog2);
+        test.add(cat2);
+        test.add(dog3);
+        test.add(cat3);
+        while (!test.isDogQueueEmpty()) {
+            System.out.println(test.pollDog().getPetType());
+        }
+        while (!test.isEmpty()) {
+            System.out.println(test.pollAll().getPetType());
+        }
+    }
+    
 }
