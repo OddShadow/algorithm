064473e26dffa5d9986bfec19950d68c23e195c3
Rick <mymiaojirui@gmail.com>
2023-12-09 20:17:39 +0800

    note

--- learn/base/class02/Code01_MergeSort.java
+++ learn/base/class02/Code01_MergeSort.java
@@ -11,24 +11,25 @@ public class Code01_MergeSort {
         }
         mergeSort(arr, 0, arr.length - 1);
     }
-    
+
     public static void mergeSort(int[] arr, int l, int r) {
         if (l == r) {
             return;
         }
         int mid = l + ((r - l) >> 1);
-        mergeSort(arr, l, mid);
+        mergeSort(arr, l, mid); // 这里mid不能mid-1,l=0,mid=0,r=1,越界
         mergeSort(arr, mid + 1, r);
         merge(arr, l, mid, r);
     }
-    
-    public static void merge(int[] arr, int l, int m, int r) {
+
+    public static void merge(int[]arr, int l, int m, int r) {
         int[] help = new int[r - l + 1];
         int i = 0;
         int p1 = l;
         int p2 = m + 1;
         while (p1 <= m && p2 <= r) {
-            help[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
+            // 带上等于，保证左边优先拷贝到左边，保证稳定性
+            help[i++] = arr[p1] <= arr[p2] ? arr[p1++] : arr[p2++];
         }
         while (p1 <= m) {
             help[i++] = arr[p1++];
