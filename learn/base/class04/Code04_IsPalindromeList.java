95a078bbb03b14f3915796713e5d83a05c79de47
Rick <mymiaojirui@gmail.com>
2023-12-10 22:11:21 +0800

    note

--- learn/base/class04/Code04_IsPalindromeList.java
+++ learn/base/class04/Code04_IsPalindromeList.java
@@ -2,31 +2,26 @@ package learn.base.class04;
 
 import java.util.Stack;
 
-//判断链表是否是回文结构
+/*
+    判断链表是否是回文结构
+ */
 public class Code04_IsPalindromeList {
-    
     public static class Node {
         public int value;
         public Node next;
-        
         public Node(int data) {
             this.value = data;
         }
     }
-    
-    // need n extra space
-    //需要n的额外空间
+
+    // O(n) 额外空间复杂度
     public static boolean isPalindrome1(Node head) {
-        //堆结构
-        Stack<Node> stack = new Stack<Node>();
-        //新建cur指向头节点
+        Stack<Node> stack = new Stack<>();
         Node cur = head;
-        //遍历将链表推进堆中
         while (cur != null) {
             stack.push(cur);
             cur = cur.next;
         }
-        //遍历头节点依次与stack弹出的值作比较
         while (head != null) {
             if (head.value != stack.pop().value) {
                 return false;
@@ -35,28 +30,23 @@ public class Code04_IsPalindromeList {
         }
         return true;
     }
-    
-    // need n/2 extra space
-    //需要n/2的额外空间
+
+    // O(n/2) 额外空间复杂度，快慢指针法
     public static boolean isPalindrome2(Node head) {
         if (head == null || head.next == null) {
             return true;
         }
-        //right为慢指针
-        //cur为快指针
-        Node right = head.next;
-        Node cur = head;
-        while (cur.next != null && cur.next.next != null) {
-            right = right.next;
-            cur = cur.next.next;
+        Node slow = head.next;
+        Node fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        Stack<Node> stack = new Stack<Node>();
-        //当慢指针不为空，将当前至最后的节点都推入堆中
-        while (right != null) {
-            stack.push(right);
-            right = right.next;
+        Stack<Node> stack = new Stack<>();
+        while (slow != null) {
+            stack.push(slow);
+            slow = slow.next;
         }
-        //比较
         while (!stack.isEmpty()) {
             if (head.value != stack.pop().value) {
                 return false;
@@ -66,46 +56,41 @@ public class Code04_IsPalindromeList {
         return true;
     }
     
-    // need O(1) extra space
-    //只需要常数级别的额外空间
+    // O(1) 额外空间复杂度，快慢指针+修改链表指针
     public static boolean isPalindrome3(Node head) {
         if (head == null || head.next == null) {
             return true;
         }
         Node n1 = head;
         Node n2 = head;
-        //找到中点n1
-        while (n2.next != null && n2.next.next != null) { // find mid node
-            n1 = n1.next; // n1 -> mid
-            n2 = n2.next.next; // n2 -> end
+        while (n2.next != null && n2.next.next != null) {
+            n1 = n1.next;
+            n2 = n2.next.next;
         }
-        //修改中点右边的节点指向，使之全部指向中点
-        n2 = n1.next; // n2 -> right part first node
-        n1.next = null; // mid.next -> null
+        n2 = n1.next;
+        n1.next = null;
         Node n3 = null;
-        while (n2 != null) { // right part convert
-            n3 = n2.next; // n3 -> save next node	n3指定为n2下一位
-            n2.next = n1; // next of right node convert		n2下一位修改为n1
-            n1 = n2; // n1 move		将n1修改为n2
-            n2 = n3; // n2 move		将n2修改为n3
+        while (n2 != null) {
+            n3 = n2.next;
+            n2.next = n1;
+            n1 = n2;
+            n2 = n3;
         }
-        
-        //从链表头尾两端遍历比较
-        n3 = n1; // n3 -> save last node	n3修改为n1保存最后一个节点
-        n2 = head;// n2 -> left first node		n2指定为第一个节点
+
+        n3 = n1;
+        n2 = head;
         boolean res = true;
-        while (n1 != null && n2 != null) { // check palindrome
+        while (n1 != null && n2 != null) {
             if (n1.value != n2.value) {
                 res = false;
                 break;
             }
-            n1 = n1.next; // left to mid
-            n2 = n2.next; // right to mid
+            n1 = n1.next;
+            n2 = n2.next;
         }
         n1 = n3.next;
         n3.next = null;
-        //还原链表
-        while (n1 != null) { // recover list
+        while (n1 != null) {
             n2 = n1.next;
             n1.next = n3;
             n3 = n1;
@@ -113,7 +98,7 @@ public class Code04_IsPalindromeList {
         }
         return res;
     }
-    
+
     public static void printLinkedList(Node node) {
         System.out.print("Linked List: ");
         while (node != null) {
@@ -122,7 +107,7 @@ public class Code04_IsPalindromeList {
         }
         System.out.println();
     }
-    
+
     public static void main(String[] args) {
         
         Node head = null;
